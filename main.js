88afe64c3884013fc6a508e283a678ef237b5c43
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-09-11 10:00:43 +0200

    Add game logic and fix variable declarations

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let playerChoiceIndex = 0;
+let playerChoiceProcessed = "";
 
 while(playerChoiceIndex === 0){
     let playerChoice = prompt("Input your choice (Rock, Paper or Scissors)");
@@ -23,7 +24,9 @@ while(playerChoiceIndex === 0){
 
 console.log("Your choice is: " + playerChoiceProcessed);
 
+let computerChoice = "";
 let computerChoiceIndex = Math.ceil(Math.random() * 3);
+
 switch(computerChoiceIndex){
     case 1:
         computerChoice = "rock";
@@ -37,3 +40,30 @@ switch(computerChoiceIndex){
 }
 
 console.log("Computer choice is: " + computerChoice);
+
+let playerScore = 0;
+let computerScore = 0;
+
+if(playerChoiceIndex === computerChoiceIndex){
+    console.log("You ended in a draw");
+    playerScore = playerScore + 1;
+    computerScore = computerScore + 1;
+} else if (playerChoiceIndex === 1 && computerChoiceIndex === 2){ // rock vs paper
+    console.log("Computer won this round");
+    computerScore = computerScore + 1;
+} else if (playerChoiceIndex === 1 && computerChoiceIndex === 3){ // rock vs scissors
+    console.log("You won this round");
+    playerScore = playerScore + 1;
+} else if (playerChoiceIndex === 2 && computerChoiceIndex === 1){ // paper vs rock
+    console.log("You won this round");
+    playerScore = playerScore + 1;
+} else if (playerChoiceIndex === 2 && computerChoiceIndex === 3){ // paper vs scissors
+    console.log("Computer won this round");
+    computerScore = computerScore + 1;
+} else if (playerChoiceIndex === 3 && computerChoiceIndex === 1){ // scissors vs rock
+    console.log("Computer won this round");
+    computerScore = computerScore + 1;
+} else if (playerChoiceIndex === 3 && computerChoiceIndex === 2){ // scissors vs paper
+    console.log("You won this round");
+    playerScore = playerScore + 1;
+}
