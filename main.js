534263efa1dc18e5cea15423e8956f75b362ba3c
Daniel Serantes <221082330+Searindor-Top@users.noreply.github.com>
2025-10-06 15:35:41 +0200

    Add skeleton UI and main changes to JS code

--- main.js
+++ main.js
@@ -1,50 +1,91 @@
+buttonRock = document.querySelector("#button-rock");
+buttonPaper = document.querySelector("#button-paper");
+buttonScissors = document.querySelector("#button-scissors");
+
+let playerChoiceIndex = 0;
+let gameRound = 1;
+
+buttonRock.addEventListener("click", function(){
+    playerChoiceIndex = 1;
+    mainGame(playerChoiceIndex);
+});
+buttonPaper.addEventListener("click", function(){
+
+    playerChoiceIndex = 2;
+    mainGame(playerChoiceIndex);
+});
+buttonScissors.addEventListener("click", function(){
+    playerChoiceIndex = 3;
+    mainGame(playerChoiceIndex);
+});
+
 let playerScore = 0;
 let computerScore = 0;
 
 let scoreArray = [playerScore, computerScore];
 
-let gameRound = 1;
-while (gameRound <= 5){
-    console.log("Round: " + gameRound);
-    let choiceArray = getChoices(); // choiceArray[0] = playerChoice, choiceArray[1] = computerchoice
-    scoreArray = gameLogic(choiceArray[0], choiceArray[1], scoreArray[0], scoreArray[1]);
-    
-    console.log("Your score is : " + scoreArray[0]);
-    console.log("Computer score is : " + scoreArray[1]);
-    console.log("");
-    gameRound++;
+
+function mainGame(playerChoiceIndex){
+    if(gameRound <= 5){
+        resultDiv = document.querySelector("#game-result");
+        roundContentDiv = document.createElement("div");
+        roundContentDiv.classList.add("round-content");
+        roundContentDiv.classList.add("flex");
+        roundContentDiv.classList.add("m-col");
+
+
+        if(gameRound == 1){
+            document.querySelector("#choose").style.display = "none";
+            gameResultH3 = document.querySelectorAll("#game-result>h3:not(#choose)");
+            gameResultH3.forEach(element => {
+                element.classList.toggle("m-hidden");
+            });
+        }
+        //roundContentDiv.innerHTML += "<h2>Round number: " + gameRound + "</h2>";
+        document.querySelector("#game-round").innerText = gameRound;
+        let choiceArray = getChoices(playerChoiceIndex, roundContentDiv); // choiceArray[0] = playerChoice, choiceArray[1] = computerchoice
+        scoreArray = gameLogic(choiceArray[0], choiceArray[1], scoreArray[0], scoreArray[1]);
+        document.querySelector("#round-result").innerText = scoreArray[2];
+        document.querySelector("#player-score").innerText = scoreArray[0];
+        document.querySelector("#computer-score").innerText = scoreArray[1];
+
+        if(gameRound == 5){
+            document.querySelector("#choose").style.display = "block";
+            if(scoreArray[0] > scoreArray[1]){
+                document.querySelector("#choose").innerText = "You won!";
+            } else if(scoreArray[0] < scoreArray[1]){
+                document.querySelector("#choose").innerText = "Computer won!";
+            } else {
+                document.querySelector("#choose").innerText = "It's a tie!";
+            }
+            gameResultH3 = document.querySelectorAll("#game-result>h3:not(#choose)");
+            gameResultH3.forEach(element => {
+                element.classList.toggle("m-hidden");
+            });
+        }
+        gameRound++;
+    }
 }
 
-function getChoices(){
-    let playerChoiceIndex = 0;
-    let playerChoiceProcessed = "";
-
-    while(playerChoiceIndex === 0){
-        let playerChoice = null;
-        while(playerChoice === null){
-            playerChoice = prompt("Input your choice (Rock, Paper or Scissors)");
-        }
-        playerChoiceProcessed = playerChoice.toLowerCase();
-
-        switch(playerChoiceProcessed){
-            case "rock":
-                playerChoiceIndex = 1;
-                break;
-            case "paper":
-                playerChoiceIndex = 2;
-                break;
-            case "scissors":
-                playerChoiceIndex = 3;
-                break;
-            default:
-                playerChoiceIndex = 0;
-                alert("Please input Rock, Paper or Scissors");
-                break;
-        }
+function getChoices(playerChoiceIndex){
+    let resultDiv = document.querySelector("#game-result");
+    switch(playerChoiceIndex){
+        case 1:
+            playerChoiceMeaning = "rock";
+            break;
+        case 2:
+            playerChoiceMeaning = "paper";
+            break;
+        case 3:
+            playerChoiceMeaning = "scissors";
+            break;
+        default:
+            playerChoiceMeaning = "error";
+            break;
     }
 
-    console.log("Your choice is: " + playerChoiceProcessed);
-
+    //roundContentDiv.innerHTML += "<h2>Your choice is: " + playerChoiceMeaning + "</h2>";
+    document.querySelector("#player-choice").innerText = playerChoiceMeaning;
     let computerChoice = "";
     let computerChoiceIndex = Math.ceil(Math.random() * 3);
 
@@ -60,35 +101,36 @@ function getChoices(){
             break;
     }
 
-    console.log("Computer choice is: " + computerChoice);
-
+    //roundContentDiv.innerHTML += "<h2>Computer choice is: " + computerChoice +"</h2>";
+    document.querySelector("#computer-choice").innerText = computerChoice;
     return [playerChoiceIndex, computerChoiceIndex]
 }
 
 
 function gameLogic(playerChoiceIndex, computerChoiceIndex, playerScore, computerScore){
+    roundContentDiv = document.querySelector("#round-result");
     if(playerChoiceIndex === computerChoiceIndex){
-        console.log("You ended in a draw");
+        resultText = "You ended in a draw";
         playerScore = playerScore + 1;
         computerScore = computerScore + 1;
     } else if (playerChoiceIndex === 1 && computerChoiceIndex === 2){ // rock vs paper
-        console.log("Computer won this round");
+        resultText = "Computer won this round";
         computerScore = computerScore + 1;
     } else if (playerChoiceIndex === 1 && computerChoiceIndex === 3){ // rock vs scissors
-        console.log("You won this round");
+        resultText = "You won this round";
         playerScore = playerScore + 1;
     } else if (playerChoiceIndex === 2 && computerChoiceIndex === 1){ // paper vs rock
-        console.log("You won this round");
+        resultText = "You won this round";
         playerScore = playerScore + 1;
     } else if (playerChoiceIndex === 2 && computerChoiceIndex === 3){ // paper vs scissors
-        console.log("Computer won this round");
+        resultText = "Computer won this round";
         computerScore = computerScore + 1;
     } else if (playerChoiceIndex === 3 && computerChoiceIndex === 1){ // scissors vs rock
-        console.log("Computer won this round");
+        resultText = "Computer won this round";
         computerScore = computerScore + 1;
     } else if (playerChoiceIndex === 3 && computerChoiceIndex === 2){ // scissors vs paper
-        console.log("You won this round");
+        resultText = "You won this round";
         playerScore = playerScore + 1;
     }
-    return [playerScore, computerScore];
+    return [playerScore, computerScore, resultText];
 }
